1382df3a0e290dd57b2fc0b6f2112d060ce52e52
Matheus de Aquino <matheusdeaquino16@gmail.com>
2026-09-18 09:09:41 -0300

    feat: initial commit - node module 2 lessons

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'no-type-imports' }
+      ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { avoidEscape: true }]
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  }
+);
